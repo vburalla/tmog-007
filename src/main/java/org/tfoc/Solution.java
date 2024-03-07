04244c36954c599674733f6631eabb5ccbf4e58b
Víctor Buralla Blasco <vburalla@mercadona.es>
2024-03-07 08:50:41 +0100

    fix: first solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,24 @@
 package org.tfoc;
 
+import java.util.Arrays;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public boolean isAnagram(
-            String s,
-            String t) {
+    public boolean isAnagram(String s, String t) {
 
-        return false;
+        boolean response = false;
+        if (s.length() == t.length()) {
+            char[] tArray = t.toCharArray();
+            Arrays.sort(tArray);
+            char[] sArray = s.toCharArray();
+            Arrays.sort(sArray);
+
+            response = Arrays.equals(sArray,tArray);
+        }
+        return response;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,30 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    Solution solution;
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @ParameterizedTest
+    @CsvSource(value =
+            {"anagram,nagaram,true",
+             "rat,car,false",
+             "aacc,ccac,false"}, delimiter = ',')
+    void testSolution(String s, String t, boolean result) {
+        assertEquals(result, solution.isAnagram(s, t));
     }
 
 }
